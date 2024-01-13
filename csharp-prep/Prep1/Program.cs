4416395f838e41a0634902fff6c2fffa2c07800c
SarmentoNathan <nathansarmento91@gmail.com>
2024-01-13 09:56:39 -0300

    Week 01 Prep 01 done

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,11 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("What is your first name?");
+        Console.Write("What is your first name? ");
         string First_Name = Console.ReadLine();
-        Console.Write("What is your last name?");
+        Console.Write("What is your last name? ");
         string Last_Name = Console.ReadLine();
 
-        Console.WriteLine($"\n\nYour name is {Last_Name}, {Last_Name} {First_Name}.");
+        Console.WriteLine($"\nYour name is {Last_Name}, {Last_Name} {First_Name}.");
     }
 }
