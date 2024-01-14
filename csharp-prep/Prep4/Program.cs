9769b5d70b015f998c4a51a88a87c4d399991f1f
SarmentoNathan <nathansarmento91@gmail.com>
2024-01-13 23:08:30 -0300

    Week 01 Prep 4 done

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,32 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        // Creating the list to be used
+        List<int> numbers = new List<int>();
+
+        // New number variable
+        int newNumber = 0;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        do
+        {
+            Console.Write("Enter Number: ");
+            newNumber = int.Parse(Console.ReadLine());
+            if (newNumber != 0)
+            {
+                numbers.Add(newNumber);
+            }
+        } while(newNumber != 0);
+
+        int soma = numbers.Sum();
+        double media = numbers.Average();
+        int maximo = numbers.Max();
+
+        Console.WriteLine($"The Sum is: {soma}\nThe average is: {media}\nThe largest number is: {maximo}");
     }
 }
