4914890e6fc4761ec70ff9b5e1322871b89c624c
SarmentoNathan <nathansarmento91@gmail.com>
2024-01-13 21:58:34 -0300

    Week 01 Prep 02 done

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your grade in percentage (don't include the % symbol): ");
+        int grade = int.Parse(Console.ReadLine()); // Convert the string given by the user into a integer
+        
+        char letter; // Create the letter variable for the grade
+
+        // Using if else statements to determine the grade letter
+        if (grade >= 90)
+        {
+            // Console.WriteLine("\nYour grade is A"); // Print grade
+            letter = 'A';
+        }
+
+        else if (grade >= 80)
+        {
+            // Console.WriteLine("\nYour grade is B"); // Print grade
+            letter = 'B';
+        }
+
+        else if (grade >= 70)
+        {
+            // Console.WriteLine("\nYour grade is C"); // Print grade
+            letter = 'C';
+        }
+
+        else if (grade >= 60)
+        {
+            // Console.WriteLine("\nYour grade is D"); // Print grade
+            letter = 'D';
+        }
+
+        else 
+        {
+            // Console.WriteLine("\nYour grade is F"); // Print grade
+            letter = 'F';
+        }
+
+        Console.WriteLine($"Your grade is {letter}");
     }
 }
