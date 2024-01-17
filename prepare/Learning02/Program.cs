a64b2e206c4ce72e75da1a10a63a935609b714af
SarmentoNathan <nathansarmento91@gmail.com>
2024-01-17 16:12:52 -0300

    Update Program.cs

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        // Creating an object from Job class
+        Job job1 = new Job();
+
+        // Changing the attributes of this object
+        job1._jobTitle = "Professor";
+        job1._company = "Uniesp Centro Universitário";
+        job1._timeInterval = "2016-2023";
+
+        // Showing the information of the attributes
+        Console.WriteLine($"{job1._jobTitle} ({job1._company}) {job1._timeInterval}");
     }
 }
