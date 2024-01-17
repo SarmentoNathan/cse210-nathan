5a1700279eaeafb28a769632f59991a7c529cd14
SarmentoNathan <nathansarmento91@gmail.com>
2024-01-17 16:51:39 -0300

    Create Resume.cs file

--- prepare/Learning02/Resume.cs
+++ prepare/Learning02/Resume.cs
@@ -4,5 +4,23 @@ public class Resume
 {
     // Creating class attributes
     public string _name;
-    public List<Job> _jobs;
+    public List<Job> _jobs = new List<Job>();
+
+    // Constructor
+    public Resume()
+    {
+
+    }
+
+    // Display Method
+    public void Display()
+    {
+        Console.WriteLine(_name);
+        Console.WriteLine("Jobs:");
+
+        foreach(Job job in _jobs)
+        {
+            job.DisplayJobDetails();
+        }
+    }
 }
