9d0ef0d3a2930d4cfd98cf32630ad3db7884ca5e
SarmentoNathan <nathansarmento91@gmail.com>
2024-01-17 15:59:41 -0300

    Create Job.cs file

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,15 @@
+// Creating the Job class
+
+public class Job
+{
+    // Creating class attributes
+    string _jobTitle = "none";
+    string _company = "none";
+    string _timeInterval = "none";
+
+    // Class constructor
+    public Job()
+    {
+        
+    }
+}
