9137ecc7d54a739608a54ef78dba17062690bbe2
SarmentoNathan <nathansarmento91@gmail.com>
2024-01-17 16:23:51 -0300

    Update Program.cs

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -9,10 +9,20 @@ class Program
 
         // Changing the attributes of this object
         job1._jobTitle = "Professor";
-        job1._company = "Uniesp Centro Universitário";
+        job1._company = "Uniesp University Centre";
         job1._timeInterval = "2016-2023";
 
         // Showing the information of the attributes
-        Console.WriteLine($"{job1._jobTitle} ({job1._company}) {job1._timeInterval}");
+        job1.DisplayJobDetails();
+
+        // Repeating the procedure to another job
+        Job job2 = new Job();
+
+        job2._jobTitle = "Substitute Professor";
+        job2._company = "Federal Institute of Pernambuco";
+        job2._timeInterval = "2021-2023";
+
+        job2.DisplayJobDetails();
+
     }
 }
