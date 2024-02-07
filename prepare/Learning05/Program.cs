7bc58ec9edf91b0c74d47b00ae227920e8829f29
SarmentoNathan <nathansarmento91@gmail.com>
2024-02-07 16:50:51 -0300

    Program.cs final version

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Square("red", 2.5));
+        shapes.Add(new Rectangle("blue", 1.2, 3.0));
+        shapes.Add(new Circle("green", 1.5));
+
+        foreach(Shape shp in shapes)
+        {
+            Console.WriteLine($"Shape color: {shp.GetColor()}");
+            Console.WriteLine($"Shape area: {shp.GetArea()}");
+        } 
     }
 }
