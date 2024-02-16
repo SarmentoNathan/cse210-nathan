8af7822e0922045552bdb3e8825de4b9687ee857
SarmentoNathan <nathansarmento91@gmail.com>
2024-02-16 02:10:16 -0300

    Program.cs file

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        bool flag = true;
+
+        while(flag)
+        {
+            Console.Clear();
+            Console.WriteLine("Menu options:\n\t1. Start Breathing Activity\n\t2. Start Reflecting Activity\n\t3. Start Listing Activity\n\t4. Quit");
+            Console.Write("Select a choice from the menu: ");
+
+            string option = Console.ReadLine();
+
+            switch(option)
+            {
+                case "1":
+                    BreathingActivity breathBreak = new BreathingActivity();
+                    breathBreak.Run();
+                    break;
+                case "2":
+                    ReflectingActivity reflectBreak = new ReflectingActivity();
+                    reflectBreak.Run();
+                    break;
+                case "3":
+                    ListingActivity listingBreak = new ListingActivity();
+                    listingBreak.Run();
+                    break;
+                case "4":
+                    flag = false;
+                    break;
+                default:
+                    Console.WriteLine("Invalid option!");
+                    break;
+            }
+        }
+
+        
     }
 }
