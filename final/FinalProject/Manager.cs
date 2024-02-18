dcbdcfde78e06a218f43ae7cdd1e032c3b75022d
SarmentoNathan <nathansarmento91@gmail.com>
2024-02-18 00:39:14 -0300

    Final Project Milestone

---
+++ final/FinalProject/Manager.cs
@@ -0,0 +1,33 @@
+using Microsoft.VisualBasic;
+
+public class Manager
+{
+    /*Class attributes*/
+
+    /*Class Constructor*/
+    public Manager()
+    {
+
+    }
+
+    /*Class Methods*/
+    public void Start()
+    {
+
+    }
+
+    public void AddGuest()
+    {
+
+    }
+
+    public void AddSupplier()
+    {
+
+    }
+
+    public string GetWeddingInfo()
+    {
+        return "info";
+    }
+}
